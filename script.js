a20eda4074656c77bf4e8a8bf47cfc5e36c3274a
Jose <josealejandrogilmendez08@gmail.com>
2022-09-28 22:38:56 -0400

    Update game
    Added win/lose audios and images. Modified styles and update DOM
    manipulation script

--- script.js
+++ script.js
@@ -1,6 +1,11 @@
+// AUDIOS
+const enterAudio = new Audio('audios/enter.wav')
+const laughAudio = new Audio('audios/laugh.wav')
+const winAudio = new Audio('audios/win.wav')
+
+// START GAME SCREEN
 const startGame = document.querySelector('.start-game')
 const gameUi = document.querySelector('.game-ui')
-const enterAudio = new Audio('audios/enter.wav')
 
 const removeStartScreen = (e) =>{
     if (e.key === 'Enter') {
@@ -20,18 +25,26 @@ const removeStartScreen = (e) =>{
 }
 document.addEventListener('keydown', removeStartScreen)
 
+// GAME SCREEN
 
 let getComputerChoice = () => Math.floor(Math.random()*3) // computer choice: returns a random number between 0 and 2
 
-let buttons = document.querySelectorAll('button');
-let pUserScore = document.querySelector('.user .score')
-let pPcScore = document.querySelector('.pc .score')
+let pUserScore = document.querySelector('.user .score');//User data
+let userImg = document.querySelector('.userImg');
 let userBoard = document.querySelector('.user .board')
-let pcBoard = document.querySelector('.pc .board')
+
 let pVs = document.querySelector('.vs')
+
+let pPcScore = document.querySelector('.pc .score');//Pc data
+let pcImg = document.querySelector('.pcImg');
+let pcBoard = document.querySelector('.pc .board')
+
 let options = document.querySelector('.options')
+let buttons = document.querySelectorAll('.choice');
 let divResult = document.querySelector('.div-result')
 let pResult = document.querySelector('.game-result')
+let retry = document.querySelector('.retry')
+
 
 let computerScore = 0;
 let userScore = 0;
@@ -53,16 +66,49 @@ buttons.forEach(button => button.addEventListener('click', e=>{
     pPcScore.textContent = computerScore
 
     if(userScore == 5){
+        winAudio.currentTime = 0;
+        winAudio.play();
+        
+        pcImg.setAttribute('src', 'images/pc_dead.png')
+        
         options.classList.add('display-none')
         divResult.classList.remove('display-none')
+        pVs.classList.add('display-none')
+        retry.classList.remove('display-none')
         pResult.textContent = 'You win the game'
+
+    }else if(computerScore == 5){
+        laughAudio.currentTime = 0;
+        laughAudio.play();
+        
+        userImg.setAttribute('src', 'images/user_dead.png')
+
+        options.classList.add('display-none')
+        divResult.classList.remove('display-none')
+        pVs.classList.add('display-none')
+        retry.classList.remove('display-none')
+        pResult.textContent = 'You lose the game'
     }
 }));
 
+retry.addEventListener('click', e => {
+    computerScore = 0;
+    userScore = 0;
+    pUserScore.textContent = userScore
+    pPcScore.textContent = computerScore
+    userImg.setAttribute('src', 'images/user.png')
+    pcImg.setAttribute('src', 'images/pc.png')
 
- 
-// rock wins scissors, paper wins rock, scissors wins paper
-// rock = 0, paper = 1, scissors = 2
+    options.classList.remove('display-none')
+    divResult.classList.add('display-none')
+    pVs.classList.remove('display-none')
+    retry.classList.add('display-none')
+
+    pVs.textContent = 'VS'
+    userBoard.innerHTML = ``;
+    pcBoard.innerHTML = ``;
+})
+
 
  const playRound = (pc, player) =>{ // a round of the game
     console.log(pc, player);
@@ -80,24 +126,3 @@ buttons.forEach(button => button.addEventListener('click', e=>{
                (pc == 1) ? 'Win' : 'Tie';
     }
 }
-
-/* const game = () =>{ //the function game, better of five
-    let computerScore = 0;
-    let userScore = 0;
-
-    for (let i = 0; i < 5; i++) {
-        result = playRound(getComputerChoice(), getPlayerChoice());
-        console.log(result);
-        if (result.substring(4, 7) == 'Win') {
-            userScore++
-        } else if(result.substring(4, 8) == 'Lose'){
-            computerScore++
-        }
-
-        console.log(`User score: ${userScore}, Computer Score: ${computerScore}`);
-    }
-
-    return (userScore > computerScore) ? 'You win the game!' : (computerScore > userScore) ? 'The computer wins the game...' : 'It\'s a tie! nobody wins!';   
-} */
-
-//console.log(game());
